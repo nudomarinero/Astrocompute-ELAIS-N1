e7f74eef69c9bad8c0cf63239b3d458837a07149
José Sabater Montes <jsm@iaa.es>
2016-02-05 12:51:52 +0000

    Upgrade to WSClean 1.10

--- playbooks/roles/lofar/files/wsclean.sh
+++ playbooks/roles/lofar/files/wsclean.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd /tmp
-tar xfj wsclean-1.9.tar.bz2
-cd wsclean-1.9
+tar xfj wsclean-1.10.tar.bz2
+cd wsclean-1.10
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=/usr ..
 make
 make install
 cd /tmp
-rm -rf wsclean-1.9*
+rm -rf wsclean-1.10*
