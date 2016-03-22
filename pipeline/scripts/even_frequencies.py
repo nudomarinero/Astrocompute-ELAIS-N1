d70df19a55ba01f0940e1f6e70922bbf54b18dfb
José Sabater Montes <jsm@iaa.es>
2016-03-22 17:58:09 +0000

    Correct script

--- pipeline/scripts/even_frequencies.py
+++ pipeline/scripts/even_frequencies.py
@@ -8,6 +8,7 @@ from __future__ import print_function
 import os
 import numpy as np
 from pyrap import tables
+import sys
 
 def main(ms):
     """
@@ -19,21 +20,19 @@ def even_ms(ms):
     """
     Replace the frequencies by an even spaced array
     """
-    freq_table = os.path.join(ms, "SPECTRAL_WINDOW")
-    # TODO: Check if the table exists
-    msfr = tables.table(freq_table, readonly=False)
+    msfr = tables.table(os.path.join(ms, "SPECTRAL_WINDOW"), 
+                        readonly=False)
     frequencies = msfr.getcol('CHAN_FREQ')[0]
     n_freqs = len(frequencies)
     even_freqs_init = np.linspace(frequencies[0],
                                   frequencies[-1],
                                   n_freqs,
                                   dtype="float64")
-    # Dirty hack to pass the strong assertion
+    # Hack to pass the strong assertion
     step = even_freqs_init[1] - even_freqs_init[0]
     even_freqs_aux = np.arange(n_freqs)*step + even_freqs_init[0]
-    
-    # TODO: Check the deviation
-    even_freqs = np.expand_dims(even_freqs_init, axis=0)
+    # Update the data
+    even_freqs = np.expand_dims(even_freqs_aux, axis=0)
     msfr.putcol('CHAN_FREQ', even_freqs)
     msfr.close()
 
