741393a009a1f86b1e23bc0b08c96976467f7cb8
José Sabater Montes <jsm@iaa.es>
2016-04-05 16:57:57 +0100

    Connection

--- pipeline/scripts/get_band.py
+++ pipeline/scripts/get_band.py
@@ -3,9 +3,11 @@ import boto.utils
 import boto.ec2
 
 region = boto.utils.get_instance_metadata()['placement']['availability-zone'][:-1]
-id = boto.utils.get_instance_metadata()['instance-id']
+iid = boto.utils.get_instance_metadata()['instance-id']
+
+conn = boto.ec2.connect_to_region(region)
 
 instances = conn.get_only_instances()
 for instance in instances:
-    if instance.id == id:
+    if instance.id == iid:
         print(instance.tags.get("band", None))
