596501241a9e82e63e38d402085258c4a3f490c2
José Sabater Montes <jsm@iaa.es>
2016-04-29 16:39:13 +0100

    Add reading tags capacity

--- pipeline/scripts/get_band.py
+++ pipeline/scripts/get_band.py
@@ -2,12 +2,28 @@ from __future__ import print_function
 import boto.utils
 import boto.ec2
 
-region = boto.utils.get_instance_metadata()['placement']['availability-zone'][:-1]
-iid = boto.utils.get_instance_metadata()['instance-id']
+def get_tag(tag_name):
+    """
+    Get the content of a tag for the current instance.
+    """
+    region = boto.utils.get_instance_metadata()['placement']['availability-zone'][:-1]
+    iid = boto.utils.get_instance_metadata()['instance-id']
 
-conn = boto.ec2.connect_to_region(region)
+    conn = boto.ec2.connect_to_region(region)
 
-instances = conn.get_only_instances()
-for instance in instances:
-    if instance.id == iid:
-        print(instance.tags.get("band", None))
+    instances = conn.get_only_instances()
+    for instance in instances:
+        if instance.id == iid:
+            return instance.tags.get(tag_name, None)
+
+def get_band():
+    """
+    Get the band number for the current instance.
+    The band number should be stored in a tag called "band"
+    """
+    get_tag("band")
+   
+if __name__ == "__main__":
+    band = get_band()
+    print(band)
+    
