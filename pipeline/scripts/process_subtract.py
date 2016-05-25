1ab25585cae7b505b0ae4054fd801e9ce43095e2
José Sabater Montes <jsm@iaa.es>
2016-05-25 13:24:20 +0100

    Improve test on subtract in one group

--- pipeline/scripts/process_subtract.py
+++ pipeline/scripts/process_subtract.py
@@ -2,29 +2,90 @@ from __future__ import print_function
 import boto.utils
 import boto.ec2
 import sh
+from sh import sudo
 from get_band import get_tag
+import boto.sns
+import logging
+import traceback
+import ConfigParser
 
 
+#logging.basicConfig(
+    #filename="pipeline.log", 
+    #level=logging.DEBUG)
+
+# General configuration
+config = ConfigParser.ConfigParser()
+config.read('/home/ubuntu/.sns_conf')
+topicarn = config.get("arn", "topicarn")
+
+# General information
+region = boto.utils.get_instance_metadata()['placement']['availability-zone'][:-1]
+iid = boto.utils.get_instance_metadata()['instance-id']
+band = get_tag("band")
+dataset = get_tag("dataset")
+
+
+# Auxiliary functions
+def notify(message, subject=None):
+    """
+    Notify the user using sns
+    """
+    snsc = boto.sns.connect_to_region(region)
+    if subject is None:
+        subject = "Notification LOFAR AWS" 
+    publication = snsc.publish(topicarn, message, subject=subject)
+
+def launch(step):
+    """
+    Launch one of the steps but notify the user if there is an error
+    """
+    try:
+        step()
+    except:
+        fname = step.__name__
+        error_message = traceback.format_exc()
+        message = ("Exception in LOFAR AWS subtract pipeline\n"+
+            "Dataset: {}\n".format(dataset)+
+            "Band:    {}\n".format(band)+
+            "Step:    {}\n".format(fname)+
+            "Error:\n"+
+            error_message
+            )
+        notify(message, 
+               subject="Exception LOFAR AWS subtract; step: {}; band: {}".format(fname, band))
+        raise
+    
+    
+## Steps
 def download_data():
     """
     Download the pretarget data using the script created
     """
+    # TODO: Initial checks and cleaning
     download = sh.Command("/home/ubuntu/download_data_pretarget.sh")
-    download()
+    for line in download(_iter=True):
+        print(line)
+    # TODO: Final checks and notification
 
 def run_pipeline():
     """
     Run the generic pipeline and send notification in case of error
     """
-    #pipeline = sh.Command("")
-    pass
+    pipeline = sh.Command("/opt/LofIm/bin/genericpipeline.py",
+                          "-c",
+                          "/home/ubuntu/astrocompute/pipeline/generic_pipeline/pipeline.cfg")
+    for line in pipeline("/home/ubuntu/astrocompute/pipeline/generic_pipeline/pre_facet_subtract.parset",
+                         _iter=True):
+        print(line)
 
 def upload_data():
     """
     Upload the computed data
     """
     upload = sh.Command("/home/ubuntu/upload_data_subtract.sh")
-    upload()
+    for line in upload(_iter=True):
+        print(line)
 
 def umount_and_remove_disk(remove=False):
     """
@@ -36,16 +97,14 @@ def terminate_instance():
     """
     Terminate the instance
     """
-    region = boto.utils.get_instance_metadata()['placement']['availability-zone'][:-1]
-    iid = boto.utils.get_instance_metadata()['instance-id']
     conn = boto.ec2.connect_to_region(region)
     conn.terminate_instances(instance_ids=[iid])
-          
+
 
 if __name__ == "__main__":
-    download_data()
-    # download data
-    # run the pipeline
-    # upload data
-    # umount and remove disk
-    # terminate instance
+    launch(download_data)
+    launch(run_pipeline)
+    launch(upload_data)
+    #launch(umount_and_remove_disk) # Not implemented yet
+    launch(terminate_instance)
+
