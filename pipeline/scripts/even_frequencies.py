d23e55b240f6e7f9f5c99605554a122ff9959449
José Sabater Montes <jsm@iaa.es>
2016-07-06 16:01:52 +0100

    New command line options

--- pipeline/scripts/even_frequencies.py
+++ pipeline/scripts/even_frequencies.py
@@ -8,7 +8,7 @@ from __future__ import print_function
 import os
 import numpy as np
 from pyrap import tables
-import sys
+import argparse
 
 def main(ms, group=5, edge_up=2, edge_down=2, force=False):
     """
@@ -39,6 +39,24 @@ def even_ms(ms, group=5, edge_up=2, edge_down=2, force=False):
         even_freqs = np.expand_dims(even_freqs_aux, axis=0)
         msfr.putcol('CHAN_FREQ', even_freqs)
         
+    msfr.close()
+
+
+def show_ms(ms, group=5, edge_up=2, edge_down=2, force=False):
+    """
+    Show the frequencies by an even spaced array.
+    If the spacing is complex and known some additional parameters can
+    be used to allow a better spacing.
+    """
+    msfr = tables.table(os.path.join(ms, "SPECTRAL_WINDOW"), 
+                        readonly=True)
+    frequencies = msfr.getcol('CHAN_FREQ')[0]
+    even_freqs_aux = get_even_frequencies(frequencies,
+                                          group=group, 
+                                          edge_up=edge_up, 
+                                          edge_down=edge_down)
+    print(frequencies)
+    print(even_freqs_aux)
     msfr.close()
 
 def get_even_frequencies(frequencies, group=5, edge_up=2, edge_down=2, ch_per_sb=64):
@@ -73,5 +91,23 @@ def check_assertion(frequencies):
     
 
 if __name__ == "__main__":
-    msname = str(sys.argv[1])
+    parser = argparse.ArgumentParser(description='Even the frequency in a MS file')
+    parser.add_argument('-f', '--force', action='store_true', help='Force update')
+    parser.add_argument('-n', '--dry-run', action='store_true', help='Just show the results')
+    parser.add_argument('-g', '--group', type=int, default=5, help='Number of channels per sb (default: 5)')
+    parser.add_argument('-d', '--edge-down', type=int, default=2, help='Channels removed from the lower edge (default: 2)')
+    parser.add_argument('-u', '--edge-up', type=int, default=2, help='Channels removed from the upper edge (default: 2)')
+    parser.add_argument('ms', help='MS or directory')
+    args = parser.parse_args()
     
+    if args.dry_run:
+        show_ms(args.ms, 
+                group=args.group, 
+                edge_up=args.edge_up, 
+                edge_down=args.edge_down)
+    else:
+        even_ms(args.ms, 
+                group=args.group, 
+                edge_up=args.edge_up, 
+                edge_down=args.edge_down,
+                force=args.force)
