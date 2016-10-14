667f8a1e2b03912facc66fb7a78f3e28d4aab69c
José Sabater Montes <jsm@iaa.es>
2016-10-14 10:46:23 +0100

    Update WSClean to 1.12

--- playbooks/roles/lofar/files/wsclean.sh
+++ playbooks/roles/lofar/files/wsclean.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd /tmp
-tar xfj wsclean-1.11.tar.bz2
-cd wsclean-1.11
+tar xfj wsclean-1.12.tar.bz2
+cd wsclean-1.12
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=/usr ..
 make
 make install
 cd /tmp
-rm -rf wsclean-1.11*
+rm -rf wsclean-1.12*
