901e9f783c48e796ec5fa5b41196016d2ebb414e
José Sabater Montes <jsm@iaa.es>
2016-12-01 22:42:31 +0000

    WSClean 2.0 role

--- playbooks/roles/wsclean/files/wsclean.sh
+++ playbooks/roles/wsclean/files/wsclean.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd /tmp
-tar xfj wsclean-1.12.tar.bz2
-cd wsclean-1.12
+tar xfj wsclean-2.0.tar.bz2
+cd wsclean-2.0
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=/usr ..
 make
 make install
 cd /tmp
-rm -rf wsclean-1.12*
+rm -rf wsclean-2.0*
