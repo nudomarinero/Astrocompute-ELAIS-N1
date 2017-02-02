94e3ebbb23851775ac1e5c4d7cb4145c80c6924c
José Sabater Montes <jsm@iaa.es>
2017-02-02 16:00:48 +0000

    Upload LOFAR role to last version of WSClean

--- playbooks/roles/lofar/files/wsclean.sh
+++ playbooks/roles/lofar/files/wsclean.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd /tmp
-tar xfj wsclean-2.0.tar.bz2
-cd wsclean-2.0
+tar xfj wsclean-2.2.1.tar.bz2
+cd wsclean-2.2.1
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=/usr ..
 make
 make install
 cd /tmp
-rm -rf wsclean-2.0*
+rm -rf wsclean-2.2.1*
