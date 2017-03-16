f4dd1c0201eadfc9d1e9dbdb51d0db850e8e23a2
José Sabater Montes <jsm@iaa.es>
2017-03-16 15:32:31 +0000

    Correct path

--- pipeline/scripts/correct_data_prefactor.sh
+++ pipeline/scripts/correct_data_prefactor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Correct the frequencies of the data in the directory specified
-for i in $(ls -1d ${1}*.${2:-dppp}); 
+for i in $(ls -1d ${1}/*.${2:-dppp}); 
 do
 python /home/ubuntu/astrocompute/pipeline/scripts/check_frequencies.py -c -w -r -t $i;
 done
