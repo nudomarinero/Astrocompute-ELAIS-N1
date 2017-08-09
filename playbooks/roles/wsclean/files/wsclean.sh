18db06d3ee11a57277705f606f84ad20c639fbec
José Sabater Montes <jsm@iaa.es>
2017-08-09 13:47:53 +0100

    Update wsclean to 2.4

--- playbooks/roles/wsclean/files/wsclean.sh
+++ playbooks/roles/wsclean/files/wsclean.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd /tmp
-tar xfj wsclean-2.2.1.tar.bz2
-cd wsclean-2.2.1
+tar xfj wsclean-2.4.tar.bz2
+cd wsclean-2.4
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=/usr ..
 make
 make install
 cd /tmp
-rm -rf wsclean-2.2.1*
+rm -rf wsclean-2.4*
