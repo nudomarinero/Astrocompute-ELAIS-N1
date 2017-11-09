dbebc1fdc0d60be9efccb9a95cc6213ad8ff3c92
José Sabater Montes <jsm@iaa.es>
2017-11-09 16:49:36 +0000

    Empirical method to split bands

--- pipeline/data/split_bands.py
+++ pipeline/data/split_bands.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 from itertools import izip_longest
 import os
+import re
 
 def grouper(n, iterable, fillvalue=None):
     """
@@ -11,16 +12,42 @@ def grouper(n, iterable, fillvalue=None):
     args = [iter(iterable)] * n
     return izip_longest(fillvalue=fillvalue, *args)
 
-def main(path, data_file_name, n=40):
+def empirical_grouper(lines, n=40):
+    """
+    Empirical version of the grouper that considers the actual numbering
+    of the bands
+    """
+    bands = {0:"", 1:"", 2:"", 3:"", 4:"", 
+             5:"", 6:"", 7:"", 8:"", 9:""}
+    for line in lines:
+        sb = re.findall("SAP000_SB(\d\d\d)_uv", line)[0]
+        sbi = int(sb)
+        if sbi == 320:
+            continue
+        if sbi > 320:
+            sbi = sbi-1
+        print(line[:-1], sbi)
+        band = int(sbi/n)
+        bands[band] = bands[band] + line
+    return bands
+        
+
+def main(path, data_file_name, n=40, g_iter=False):
     """
     Apply the grouper to the specified file
     """
     with open("{}/{}.txt".format(path, data_file_name), "r") as data_file:
-        for i, g in enumerate(grouper(n, data_file, fillvalue='')):
-            lines = list(g)
-            lines[-1] = lines[-1][:-1] # Remove the new line from the last line
-            with open("{}/{}-BAND{}.txt".format(path, data_file_name, i), 'w') as fout:
-                fout.writelines(lines)
+        if g_iter:
+            for i, g in enumerate(grouper(n, data_file, fillvalue='')):
+                lines = list(g)
+                lines[-1] = lines[-1][:-1] # Remove the new line from the last line
+                with open("{}/{}-BAND{}.txt".format(path, data_file_name, i), 'w') as fout:
+                    fout.writelines(lines)
+        else:
+            bands = empirical_grouper(data_file.readlines(), n=n)
+            for i in range(10):
+                with open("{}/{}-BAND{}.txt".format(path, data_file_name, i), 'w') as fout:
+                    fout.write(bands[i])
 
 if __name__ == "__main__":
     import sys
